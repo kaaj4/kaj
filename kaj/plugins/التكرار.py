5d886441b911d14e889c3eddfc83f639adb8f1ef
kaaj4 <102202400+kaaj4@users.noreply.github.com>
2022-06-13 14:54:33 +0300

    Update التكرار.py

--- kaj/plugins/التكرار.py
+++ kaj/plugins/التكرار.py
@@ -4,12 +4,10 @@ from telethon.tl import functions, types
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-#from plugins.helpers import _knoutils
 from iqthon import iqthon
-
 @iqthon.on(events.NewMessage(outgoing=True, pattern="^.تكرار (.*)"))
 async def spammer(event):
-    jasem = await event.get_reply_message()
+    kno1 = await event.get_reply_message()
     kno = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
     counter = int(kno[0])
     if counter > 50:
@@ -19,10 +17,7 @@ async def spammer(event):
         sleeptimet = 0.1
         sleeptimem = 0.3
     await event.delete()
-    await spam_function(event, jasem, kno, sleeptimem, sleeptimet)
-
-
-
+    await spam_function(event, kno1, kno, sleeptimem, sleeptimet)
 @iqthon.on(events.NewMessage(outgoing=True, pattern="^.مؤقت (.*)"))
 async def spammer(event):
     reply = await event.get_reply_message()
@@ -31,27 +26,24 @@ async def spammer(event):
     kno = input_str[1:]
     await event.delete()
     await spam_function(event, reply, kno, sleeptimem, sleeptimet, DelaySpam=True)
-  
-async def spam_function(event, jasem, kno, sleeptimem, sleeptimet, DelaySpam=False):
+async def spam_function(event, kno1, kno, sleeptimem, sleeptimet, DelaySpam=False):
     hmm = base64.b64decode("VHdIUHd6RlpkYkNJR1duTg==")
     counter = int(kno[0])
     if len(kno) == 2:
         spam_message = str(kno[1])
         for _ in range(counter):
             if event.reply_to_msg_id:
-                await jasem.reply(spam_message)
+                await kno1.reply(spam_message)
             else:
                 await event.client.send_message(event.chat_id, spam_message)
             await asyncio.sleep(sleeptimet)
-    elif event.reply_to_msg_id and jasem.media:
+    elif event.reply_to_msg_id and kno1.media:
         for _ in range(counter):
-            jasem = await event.client.send_file(
-                event.chat_id, jasem, caption=jasem.text
-            )
-            await _knoutils.unsavegif(event, jasem)
+            kno1 = await event.client.send_file(event.chat_id, kno1, caption=kno1.text)
+            await _knoutils.unsavegif(event, kno1)
             await asyncio.sleep(sleeptimem)
-    elif event.reply_to_msg_id and jasem.text:
-        spam_message = jasem.text
+    elif event.reply_to_msg_id and kno1.text:
+        spam_message = kno1.text
         for _ in range(counter):
             await event.client.send_message(event.chat_id, spam_message)
             await asyncio.sleep(sleeptimet)
