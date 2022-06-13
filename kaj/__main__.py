182f5fd633bbfd10647e5faa1253c27e07f522a4
kaaj4 <102202400+kaaj4@users.noreply.github.com>
2022-06-13 14:50:54 +0300

    Update __main__.py

--- kaj/__main__.py
+++ kaj/__main__.py
@@ -5,45 +5,38 @@ import logging
 import importlib
 from pathlib import Path
 from telethon import TelegramClient, events
-from iqthon import iqthon, LOGGER
+from kaj import kaj, LOGGER
 from telethon.tl.functions.channels import JoinChannelRequest
-from iqthon.plugins import *
-
+from kaj.plugins import *
 async def saves():
     try:
-        os.environ["STRING_SESSION"] = "**@IQTHON**"
+        os.environ["STRING_SESSION"] = "**@kaj**"
     except Exception as e:
         print(str(e))
     try:
-        await iqthon(JoinChannelRequest("@iqthon"))
+        await kaj(JoinChannelRequest("@kaj"))
     except BaseException:
         pass
     try:
-        await iqthon(JoinChannelRequest("@telethonmusic"))
+        await kaj(JoinChannelRequest("@telethonmusic"))
     except BaseException:
         pass
-
 def load_plugins(plugin_name):
-    path = Path(f"iqthon/plugins/{plugin_name}.py")
-    name = "iqthon.plugins.{}".format(plugin_name)
+    path = Path(f"kaj/plugins/{plugin_name}.py")
+    name = "kaj.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["iqthon.plugins." + plugin_name] = load
-
-path = "iqthon/plugins/*.py"
+    sys.modules["kaj.plugins." + plugin_name] = load
+path = "kaj/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
-
-iqthon.start()
-
-iqthon.loop.create_task(saves())
-
-print("- تم بنجاح تنصيب سورس تليثون  @iqthon")
-
-iqthon.run_until_disconnected()
+kaj.start()
+kaj.loop.create_task(saves())
+print("- تم بنجاح تنصيب سورس سورس كاج  @kaj")
+kaj.run_until_disconnected()
